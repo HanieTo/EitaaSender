e2138dacfc930f47e06a0f54dc0780b46c01f2f3
HanieTo <hanietohidlu@gmail.com>
2025-03-13 22:20:22 +0330

    Add files via upload

---
+++ eitaa_myself/eitaa_myself/BotService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static System.Net.WebRequestMethods;
+
+namespace eitaa_myself
+{
+  
+    class BotService
+    {
+        private readonly string _token;
+        private readonly string _chatId;
+        
+       
+
+        public BotService(string token, string chatId)
+        {
+            _token = token;
+            _chatId = chatId;
+        
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            string url = $"https://eitaayar.ir/api/{_token}/sendMessage?chat_id={_chatId}&text={Uri.UnescapeDataString(message)}&date=0&parse_mode=&pin=&viewCountForDelete=";
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                string responseText = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine($"Server Response:{responseText}");
+            }
+
+        }
+        public async Task SendFileAsync(string filePath)
+        {
+            string url = $"https://eitaayar.ir/api/{_token}/sendFile";
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                Console.WriteLine("Error: file not found.");
+                return;
+            }
+
+            using (HttpClient client = new HttpClient())
+            using (MultipartFormDataContent form = new MultipartFormDataContent())
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            using (StreamContent fileContent = new StreamContent(fs))
+            {
+                form.Add(new StringContent(_chatId), "chat_id");
+                form.Add(new StringContent("Sending a file"), "title");
+                form.Add(new StringContent("here is your file"), "caption");
+                form.Add(fileContent, "file", Path.GetFileName(filePath));
+
+                HttpResponseMessage response = await client.PostAsync(url, form);
+                string responseText = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine($"Server Response:{responseText}");
+            }
+        }
+
+    }
+
+
+
+}
+
---
+++ eitaa_myself/eitaa_myself/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eitaa_myself
+{
+    class Program
+    {
+
+        static async Task Main()
+        {
+
+
+            Console.WriteLine("Please choose: \n1 .send text \n 2.send file \n type 1 or 2");
+            string Choose = Console.ReadLine();
+
+
+            Console.WriteLine("Write your token");
+            string token = Console.ReadLine();
+
+            Console.WriteLine("Write your chat id");
+            string chatId = Console.ReadLine();
+
+
+            BotService botService = new BotService(token, chatId);
+
+
+            if (Choose == "1")
+            {
+                Console.WriteLine("Write your message");
+                string message = Console.ReadLine();
+
+                await botService.SendMessageAsync(message);
+            }
+            else if (Choose == "2")
+            {
+
+                Console.WriteLine("Write your filePath");
+                string filePath = Console.ReadLine();
+
+                await botService.SendFileAsync(filePath);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please restart the program.");
+            }
+
+        }
+
+
+
+    }
+}
+
